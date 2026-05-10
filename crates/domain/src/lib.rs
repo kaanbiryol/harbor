b44b606d085bebfe06a494463aacf91be7aef0d5
Kaan Biryol <dev.biryolkaan@gmail.com>
2026-05-10 14:44:20 +0200

    refactor: move diff mapping to domain

--- crates/domain/src/lib.rs
+++ crates/domain/src/lib.rs
@@ -1,6 +1,8 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
+pub mod diff;
+pub mod diff_reviews;
 pub mod reviews;
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
